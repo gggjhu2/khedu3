50ea8b643dcb2ccada79e5918926d3bb03e3a64e
gggjhu2 <gggjhu2@gmail.com>
2020-12-31 19:50:46 +0900

    6교시 종료!

--- java_workspace/13_IO/src/byre_system_file/FileIOTest.java
+++ java_workspace/13_IO/src/byre_system_file/FileIOTest.java
@@ -1,5 +1,120 @@
 package byre_system_file;
 
-public class FileIOTest {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class FileIOTest {
+public static void main(String[] args) {
+	FileIOTest ft= new FileIOTest();
+//	ft.test1();
+	
+	ft.test2();
+	System.out.println("===정상종료되었습니다====");
+}
+
+
+
+
+	
+	public void test1() {
+		
+		String filePath ="test.txt";
+		String copyPath = "byte/test_copy.txt";
+		FileInputStream fis =null;
+		//바이트기반의 입력스트림
+		
+		FileOutputStream fos =null;
+		
+		try {
+			 fis= new FileInputStream(filePath);
+			 fos = new FileOutputStream(copyPath);
+			 int data =0;//읽어온 데이터를 담을 변수
+			 
+			 while((data =fis.read()) != -1) {
+				 System.out.print((char)data );
+				 fos.write(data);
+				 
+			 }
+			 
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+				try {
+					fis.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				try {
+					fos.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			try {
+				fis.close();//널포인트익셉션이떴다.
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	
+	// 내컴퓨터의 이미지파일을 복사해서 byte폴더 하위에 위치시킬것.
+	// 경로상의 한글사용 하지말것.
+	public void test2() {
+		String fileName ="C:\\1\\1.jpg";
+		String destName = "byte/test_copy23.jpg";
+		FileInputStream fis =null;
+		FileOutputStream fos =null;
+		
+		try {
+			 fis= new FileInputStream(fileName);
+			 fos = new FileOutputStream(destName);
+			 int data =0;//읽어온 데이터를 담을 변수
+			 
+			 while((data =fis.read()) != -1) {
+				 fos.write(data);
+				 
+			 }
+			 
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+				try {
+					fis.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				try {
+					fos.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			try {
+				fis.close();//널포인트익셉션이떴다.
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("파일복사완료");
+	}
+	
+	
+	
 }
---
+++ java_workspace/13_IO/src/kh/java/io/char_/FileRWTest.java
@@ -0,0 +1,90 @@
+package kh.java.io.char_;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileRWTest {
+
+	public static void main(String[] args) {
+
+		FileRWTest f = new FileRWTest();
+		// f.test1();
+		f.test2();
+
+	}
+
+	//
+	// 문자기반 스트림
+	//
+	// char 2byte
+	//
+	private void test1() {
+
+		// 경로상의 실제파일(없을수잇다)을 가리키는 자바객체
+		File f = new File("test.txt");
+		FileReader fr = null;
+
+		try {
+
+			fr = new FileReader(f);
+
+			int data = 0;
+
+			while ((data = fr.read()) != -1) {
+				System.out.print((char) data + ":");
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+
+			try {
+				fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	// try ....whith ... resource 구문 (jdk 1.7)
+	// 보조스트림을 이용한처리
+	//
+	//
+	public void test2() {
+
+		File f = new File("test.txt");
+		File dest =new File("char/test_copy333.txt");
+		//BufferedReader br = null;
+		//BufferedReader b = null;
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(f));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(dest));
+				//트라이( ) 안에 스트링객체를 선언하면 자동 클로스까지해준다.
+				//jdsk1.7부터 추가된기능이므로 버젼에 주의한다.
+	){
+			String data =null;
+			
+			while((data = br.readLine()) !=null) {
+				
+				System.out.println(data + ":");
+				bw.write(data + "\n");
+				
+				
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
