7336660c4e56d42376aad9d8242230d68b7d5513
gggjhu2 <gggjhu2@gmail.com>
2021-01-04 15:32:13 +0900

    커밋

--- java_workspace/13_IO/src/byre_system/object/ObjectIOTest.java
+++ java_workspace/13_IO/src/byre_system/object/ObjectIOTest.java
@@ -1,8 +1,13 @@
 package byre_system.object;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Calendar;
 
@@ -11,29 +16,54 @@ public class ObjectIOTest {
 	public static void main(String[] args) {
 
 		ObjectIOTest o = new ObjectIOTest();
-		o.test1();
+		o.test1();// 쓰기
+		o.test2();// 읽ㄱ
 	}
 
 	private void test1() {
 
 		File f = new File("object/user.ser");
 
-		try (ObjectOutputStream oos =
-				new ObjectOutputStream
-				(new BufferedOutputStream
-						(new FileOutputStream(f)));
-				) {
-			
-			User u =new User("hot","1234", "dsadasd",15,Calendar.getInstance());
-			oos.writeObject(u);
+		try (ObjectOutputStream oos = new ObjectOutputStream(
+				new BufferedOutputStream(new FileOutputStream(f)));) {
+
+			User u1 = new User("hot1", "1234", "dsadasd1", 15, Calendar.getInstance());
+			User u2 = new User("hot2", "1234", "dsadasd2", 15, Calendar.getInstance());
+			User u3 = new User("hot3", "1234", "dsadasd3", 15, Calendar.getInstance());
+
+			oos.writeObject(u1);
+			oos.writeObject(u2);
+			oos.writeObject(u3);
+
 			System.out.println("객체쓰기완료!");
-			
-			
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void test2() {
+		File f = new File("object/user.ser");
+
+		try (
+
+				ObjectInputStream ois = new ObjectInputStream(
+						new BufferedInputStream(
+								new FileInputStream(f)));) {
+			while(true) {
+			System.out.println(ois.readObject());
+			
+			
+			}
+		}catch(EOFException e) {
+			System.out.println("파일 읽어오기 완료!\n(에러지만에러아니다)");
+			
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("객체불러오기완료");
+
 	}
 
 }
--- java_workspace/13_IO/src/byre_system/object/User.java
+++ java_workspace/13_IO/src/byre_system/object/User.java
@@ -18,43 +18,19 @@ public class User implements Serializable {
 	
 	
 	
+	/**
+	 * 	//클래스별 고유번호
+	 */
+	private static final long serialVersionUID = 1L;
 	private String id ;
-	private String password ;
+	//객체 입출력 스트림에서 해당값을 전송하지않는다.  transient
+	private transient String password ;
 	private String name;
 	private int age ;
 	private Calendar regDate ;
 	
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public Calendar getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Calendar regDate) {
-		this.regDate = regDate;
-	}
+	
 	public User(String id, String password, String name, int age, Calendar regDate) {
 		super();
 		this.id = id;
@@ -67,12 +43,44 @@ public class User implements Serializable {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
 	@Override
 	public String toString() {
-		return"";
-		
+		return "User [id=" + id + ", password=" + password + ", name=" + name + ", age=" + age + ", regDate=" + regDate
+				+ ", getId()=" + getId() + ", getPassword()=" + getPassword() + ", getName()=" + getName()
+				+ ", getAge()=" + getAge() + ", getRegDate()=" + getRegDate() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public Calendar getRegDate() {
+			return regDate;
+		}
+		public void setRegDate(Calendar regDate) {
+			this.regDate = regDate;
+	}
+	
 	
 	
 	
