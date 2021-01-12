8ed31a9dda5891aa8ec2a8a7382b2138ffe7e92e
gggjhu2 <gggjhu2@gmail.com>
2021-01-12 21:55:32 +0900

    2021.01.12수업끝

--- java_workspace/14_CollectionFramework/src/kh/java/collection/set/HashSetTest.java
+++ java_workspace/14_CollectionFramework/src/kh/java/collection/set/HashSetTest.java
@@ -24,23 +24,34 @@ public class HashSetTest {
 	public static void main(String[] args) {
 		
 		HashSetTest hs =new HashSetTest();
-		//hs.test1();
-		//hs.test2();
-		//hs.test3();
-		//hs.test4();
-		hs.test5();
+
+		//해쉬셋 사용하기 첫 공부 메서드
+//		hs.test1();
+		
+		//리스트의 중복을 리스트를 셋으로바꿈으로 해결할수있는방법 공부 메서드
+//		hs.test2();
+		
+		//중복제거관련해서 추가적으로연습을해보는 메서드이다. (커스텀클래스 처리)
+		hs.test3();
+		
+		
+//		hs.test4();
+//		hs.test5();
 		
 		
 	}
 
 	private void test1() {
 		
+		//다형성을 적용해서 만들수있다.
 		HashSet<Integer> set1 =new HashSet<>();
 		Set <String> set2 =new HashSet();
 		Collection<Double> set3 = new HashSet<>();
 		
+
+		//요소 추가
 		set2.add("안녕");
-//		set2.add(123);//제네릭 덕분에 다르타입의 오소는 추가가 불가능하다.
+//		set2.add(123);//제네릭 덕분에 다르타입의 오소는 추가가 불가능하다. ==>set2는 스트링만 받게 제네릭스트링설정되어잇따.
 		set2.add("hello world");
 		set2.add("zzz");
 		set2.add("zzz");
@@ -49,75 +60,116 @@ public class HashSetTest {
 		
 		
 		//저장된 요소 개수 확인
+		System.out.println("======set 사용해보기(set2에요소추가) 첫 시도=====");
+							//요소의 갯수를 알려주는 메서드
 		System.out.println("요소는 모두 "+set2.size()+"개입니다.");
 		System.out.println(set2);
+		System.out.println("==========================");
+		System.out.println("");
+		
 		
 		//인덱스 통한 요소 가져오기는 지원하지 않는다.
 		//가져올때는 반복문을 통한 전체열람만 가능하다.
 		//일반포문은 사용불가.
 		//1.포이치문은 가능하다.
-		System.out.println("===포이치문===");
-		for(String s : set2)
+		System.out.println("===포이치문을통한 접근만 가능하다 객체 일반포문사용불가.===");
+			//포이치문사용 
+		for(String s : set2) 
 		System.out.println(s);
+		System.out.println("=======================");
+		System.out.println("");
+		
 		
 		//2.iterator 객체
-		System.out.println("==이터레이터===");
+		//	(순차적접근가능하게만들어놓은 전통적인 메서드)
+		
+		System.out.println("=====이터레이터사용=====");
+							   // ㅁㅁ .iterator();를 우항에 대입해서 사용할수있다.
 		Iterator <String> iter =set2.iterator();
-		while(iter.hasNext()) {
+		//해스모어 토커나이저와 사용방식이비슷하다. 포인트를가르켜다음거를 출력하는방법이다
+		while(iter.hasNext()) 
+			{
 			String s =iter.next();
 			System.out.println(s);
-		}
-		System.out.println("===단순 총 출력===");
+			}
+		System.out.println("=========단순 총 출력 set2그냥출력==========");
 		System.out.println(set2);
+		System.out.println("=====================================");
+		System.out.println("");
 		
-		//삭제  :동등한 객체를 찾아 삭제 
+		//삭제  :동등한 객체를 찾아 삭제   (동등한객체를 찾아 삭제하고 인덱스를통한 접근의 삭제가 불가하다.)
 		//(equals &hashCOde override 가필수)
-		System.out.println("===zzz삭제===");
+		System.out.println("===============\"zzz\" 삭제하기===========");
 		set2.remove("zzz");
 		System.out.println(set2);
+		System.out.println("=============\"zzz\" 삭제 성공 ===============");
+		System.out.println("");
+		
 		
 		//전체삭제
-		System.out.println("===전체삭제===");
+		System.out.println("==========전체삭제(인덱스를통한삭제는존재하지않는다.)========");
 		set2.clear();
 		System.out.println(set2);
+		System.out.println("=========요소 전체삭제 성공============================");
+		System.out.println("");
+	
 		
 		//비어있는지 검사
-		System.out.println("===비어있는지 검사===");
+		System.out.println("=========비어있는지 검사 ㅁㅁ.isEmpty()사용하여 실행===========");
 		System.out.println(set2.isEmpty());
-		
+		System.out.println("==================비어있으므로 true출력 성공 ===============");
 	}
 
+	
+	//   List 와Set은 서로공통된조상 collection을 가지고있다.
 	//  서로 변환해서 사용할수있다.
 	//	List -> set		=> 중복을 제거할때 사용한다.
 	//					
 	//	Set  -> List	=> 정렬을 하기위해 사용한다.
-	//
 	public void test2() {
 		
+		
 		List <Integer> list =new ArrayList<>();
-		
 		list.add(1);
 		list.add(3);
 		list.add(2);
 		list.add(1);
 		list.add(3);
 		list.add(2);
+		System.out.println("============list에 1,3,2,1,3,2,를대입한다. 중복 요소 저장이가능하고\n============저장순서가 유지된다.===============");
 		System.out.println(list);
+		System.out.println("=========list여서 모든요소가 입력한그대로의 순서대로 중복도 포함 다 들어가있다.============");
+		System.out.println("");
 		
-		//겹치는것 제거 하기위해 셋으로 변환
-		System.out.println("[중복제거 를위해 셋으로 변환해본다]");
+		
+		//겹치는것 제거 하기위해 SET으로 변환
+						//해쉬셋 에 list를넣어서 list를 해쉬셋으로 변환할수있다.
+									//값대입 list값요소를 전체 대입해서 set으로변환한다
 		Set <Integer> set =new HashSet<>(list);
 										//해당메서드에 리스트를 넣는다.
+		System.out.println("=========[중복제거 를위해 셋으로 변환해본다]============");
 		System.out.println(set);
+		System.out.println("===============중복제거 성공하였다 => list가 셋으로된것이다.=======\n");
 		//중복제거 성공
 		
-		//list변환 :내림차순 정렬
-		System.out.println("내림차순 정렬 컬랙션스.소트. 리버스오더로 내림차순으로정렬해본다.");
 		
+		
+		//list변환 :내림차순 정렬
+		System.out.println("========오름차순 정렬 컬랙션스.소트. 리버스오더로 내림차순으로정렬해본다.=========");
+										//위에 생성해놓은 set을 그대로 전달해준다.
 		List <Integer> list2 = new ArrayList<>(set);
-		Collections.sort(list2, Collections.reverseOrder());
-		//list.sort ~~~~도 같은결과 다.
+				//소트는 오름차순정렬
+		System.out.println("==오름차순==");
+		Collections.sort(list2);
 		System.out.println(list2);
+		//소트를 통해 오름차순정렬을한것을 리버스오더로 뒤집어서 내림차순으로바꾼다.  
+		/*sort를통한 오름차순출력===>*/System.out.println("===내림차순==");
+		Collections.sort(list2, Collections.reverseOrder());
+		/*내린차순 출력===>*/		System.out.println(list2);
+		//list.sort ~~~~도 같은결과 다.
+		System.out.println("");
+		System.out.println("===list의 형변환 공부 예제 메소드 끗 =====");
+		System.out.println("list는 중복도 다 요소가들어간다 해당부분에서 중복된부분을\n제거하고싶을때는 set으로바꿔서해결할수있다.");
 		
 	}
 
@@ -125,23 +177,33 @@ public class HashSetTest {
 	
 	//
 	// 커스텀 클래스 중복처리
-	//
-	//
 	public void test3() {
+		//퍼슨 이없으므로 만들어준다 같은패키지에 Person.java 클래스생성==>작성해준다 따라가서 퍼슨클래스를 작성해본다.
 		
 		Set<Person> set =new HashSet<>();
 		
-		set.add(new Person("홍길동"));
-		set.add(new Person("신사임당"));
-		set.add(new Person("홍길동"));
+		Person hong = new Person("홍길동");
+		set.add(hong);// 밑밑 줄의 홍길동과는 전혀다른 주소값을가지는 "홍길동" 값만똑같은  별도의 객체이다.
+		System.out.println(hong.hashCode());
+		Person sin = new Person("신사임당");
+		set.add(sin);
+		System.out.println(sin.hashCode());
+		System.out.println(sin.equals(hong));
+		
+		Person hong2 = new Person("홍길동");
+		System.out.println(hong2.hashCode());
+		set.add(hong);//==>set은 중복이안되므로 중복 요소삽입이안된다. 그러나 위의 홍길동과 밑의 홍길동은 다른별개의 객체이다.
+		System.out.println(set.hashCode());
+									  //이럴때 홍길동 들이 같을때 해쉬코드도 동일하게해주는 hashcode &equals 오버라이드를 통해서 해결해준다.
+									  //==>Person.java 파일로 가서 오버라이드 문을 작성해주어 해결해본다.
 		//홍길동이 중복되어 삽입이된다.
-		//셋이면 중복이안되는데 삽입이되는이유는. 외부적으로는 값이같지만
+		//SET이면 중복이안되는데 삽입이되는이유는. 외부적으로는 값이같지만
 		//내부적 해시코드를봤을때는 다른 참조변수(필드값) 스트링이기때문이다.
 		//이를 해결하기위해서는 펄슨의 이퀄즈 해쉬코드를 오버라이드하여야한다. 퍼슨 클래스에서이어서진행
 		//hashcode 오버라이딩을 하지않았다.
 		//이또한해줘야한다.
+		
 		System.out.println(set);
-	
 	
 	}
 	
--- java_workspace/14_CollectionFramework/src/kh/java/collection/set/Person.java
+++ java_workspace/14_CollectionFramework/src/kh/java/collection/set/Person.java
@@ -1,8 +1,11 @@
 package kh.java.collection.set;
 
-import java.util.Objects;
-
+//HashSetTest.java 의 test3(); 에 서 퍼슨객체를 생성한다.
+//필드  String name 을만들고 기본생성자, 파라매터생성자, 게터세터 ,투스트링 오버라이드를 작성하고 본문 으로넘어간다.
+//test3() 진행중 홍길동과  홍길동의 객체가다를때 해쉬코드도 당연히다르므로 해당부분을 문자열 이같을때 해쉬코드도 같게하기위한 해쉬코드이퀄즈 오버라이드를작성해준다
+// 																				*해당구문은 37번줄부터 시작한다.*
 public class Person {
+	
 	
 	private String name;
 
@@ -29,30 +32,48 @@ public class Person {
 		return "Person [name=" + name + "]";
 	}
 	
-	//해싀코드 이퀄즈 커스텀 클래스 오버라이드
-	@Override
-	public boolean equals(Object o) {
-		
-		if(this ==o)
-			return true;
-		
-		if(o==null || !(o instanceof Person))
-		return false;
-		
-		Person other = (Person)o;
-		
-		if(name ==null) {
-			if(other.name !=null)
-				return false;
-		}else {
-			if (!name.equals(other.name))
-				return false;
-			
-		}
-		return true;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(name);
-	}
+	
+// HashSetTest.java 의 test3()메서드 진행중 홍길동의 객체 두개생성시 둘의 해쉬코드가 다른 객체이다 해당부분을 같은객체로 만들기위한 오버라이드수정부분이다.
+// 해싀코드 이퀄즈 커스텀 클래스 오버라이드
+//	@Override		//작성방법은 외워서 숙지 숙달해보자...방법이없다...이해하기는 아직너무 힘들다
+					//TODO : 해당부분에대한 각 객체가가리키는 게무엇인지 이해가 없는상황..해당부분 학우들에게 질문혹은 선생님꼐질문통해 해결해보자
+	
+	
+	//오브젝트를매개변수로받는다.
+//	public boolean equals(Object o) {
+//		
+	//객체를 비교하는부분
+//		//이부분이 뭔지모르겟다.
+//		if(this ==o)
+//			return true;
+//		
+	
+	// 비정상코드를 걸러내기위한 이프문
+//		if(o==null || !(o instanceof Person))
+//		return false;
+//		
+//		
+	
+	//필드값을 비교하는것  ==> 타입캐스팅이 필요하다.   
+//		Person other = (Person)o;
+//		if(name ==null) {
+//			if(other.name !=null)
+//				return false;
+//		}else {
+	
+			//결국은 이부분이 필드값비교하는것이다.
+//			if (!name.equals(other.name))
+//				return false;
+//			
+//		}
+//		return true;
+//	}
+//	
+
+//	@Override
+//	public int hashCode() {
+			//이퀄즈가 트루가나오면 hash코드가같게만들어주는 메서드이다.
+//		return Objects.hash(name);
+//	}
+	
 }
